4aaafa2edf1ad15f02dd0ea4583a6c8d9dd14a12
Ariel Souza <arielsvadev@gmail.com>
2025-06-28 20:47:27 -0300

    feat(appointment): add get_available_times utility function

---
+++ appointment/utils.py
@@ -0,0 +1,18 @@
+from typing import Iterable
+from datetime import date, datetime, timedelta, timezone
+
+from appointment.models import Appointment
+
+def get_available_times(date: date) -> Iterable[datetime]:
+    start = datetime(year=date.year, month=date.year, day=date.day, hour=9, minute=0, tzinfo=timezone.utc)
+    end = datetime(year=date.year, month=date.year, day=date.day, hour=17, minute=30, tzinfo=timezone.utc)
+    delta = timedelta(minutes=30)
+
+    available_times = set()
+
+    while start < end:
+        if not Appointment.objects.filter(date_hour=start).exists():
+            available_times.add(start)
+        start += delta
+
+    return available_times
