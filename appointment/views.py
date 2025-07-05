e6a2e46650f3cf1a747c68d54e15aab2c5a16984
Ariel Souza <arielsvadev@gmail.com>
2025-07-05 07:33:38 -0300

    feat(appointment): create get_times view for daily availability

--- appointment/views.py
+++ appointment/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from datetime import datetime
+
+from appointment.utils import get_available_times
+
+
+@api_view(http_method_names=["GET"])
+def get_times(request):
+    date =  request.query_params.get('date')
+
+    if not date:
+        date = datetime.now().date()
+    else:
+        date = datetime.fromisoformat(date).date()
+
+    available_times = sorted(list(get_available_times(date)))
+
+    return JsonResponse(available_times, safe=False)
