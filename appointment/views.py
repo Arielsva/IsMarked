185d25bb3bbd234e3adb55e29c711edf8a469a0d
Ariel Souza <arielsvadev@gmail.com>
2025-07-10 05:40:47 -0300

    feat(appointment): implement custom permission classes for view access control

--- appointment/views.py
+++ appointment/views.py
@@ -2,14 +2,31 @@ from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework import mixins, generics
+from rest_framework import mixins, generics, permissions
 
 from datetime import datetime
 
 from appointment.utils import get_available_times
 from appointment.serializers import AppointmentSerializer
 from appointment.models import Appointment
+
+
+class IsProviderOrCreateOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method == "POST":
+            return True
+        
+        provider = request.query_params.get("provider", None)
+        if request.user.username == provider:
+            return True
+        return False
+    
+
+class IsProvider(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if obj.provider == request.user:
+            return True
+        return False
 
 
 @api_view(http_method_names=["GET"])
@@ -25,6 +42,7 @@ def get_times(request):
 
     return JsonResponse(available_times, safe=False)
 
+
 class AppointmentDetail(
     mixins.RetrieveModelMixin,
     mixins.UpdateModelMixin,
@@ -33,6 +51,7 @@ class AppointmentDetail(
 ):
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
+    permission_classes = [IsProvider]
 
     def get(self, request, *args, **kwargs):
         return self.retrieve(request, *args, **kwargs)
@@ -53,6 +72,7 @@ class AppointmentList(
     generics.GenericAPIView
 ):
     serializer_class = AppointmentSerializer
+    permission_classes = [IsProviderOrCreateOnly]
 
     def get_queryset(self):
         provider = self.request.query_params.get("provider", None)
@@ -65,6 +85,7 @@ class AppointmentList(
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
     
+
 @api_view(http_method_names=["POST"])
 def appointment_cancel(request, pk):
     object = get_object_or_404(Appointment, id=pk)
