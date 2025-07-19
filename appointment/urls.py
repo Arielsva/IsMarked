dcf0f2b91bb8cf6f42b981d2588d1002e66821e8
Ariel Souza <arielsvadev@gmail.com>
2025-07-19 20:38:27 -0300

    refactor(appointment): update URL to use class-based AppointmentCancel view

--- appointment/urls.py
+++ appointment/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from appointment.views import get_times, AppointmentDetail, AppointmentList, ProviderList, appointment_cancel
+from appointment.views import get_times, AppointmentDetail, AppointmentList, ProviderList, AppointmentCancel
 
 urlpatterns = [
     path('times/', get_times),
     path('appointment/', AppointmentList.as_view()),
     path('appointment/<int:pk>/', AppointmentDetail.as_view()),
-    path('appointment/cancel/<int:pk>/', appointment_cancel),
+    path('appointment/cancel/<int:pk>/', AppointmentCancel.as_view()),
     path('provider/', ProviderList.as_view()),
 ]
