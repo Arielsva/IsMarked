c5e5bddbed53437bfa2281873c7b647a91043539
Ariel Souza <arielsvadev@gmail.com>
2025-07-28 22:11:48 -0300

    fix(appointment): allow multiple appointments to different providers

--- appointment/utils.py
+++ appointment/utils.py
@@ -4,6 +4,7 @@ from datetime import date, datetime, timedelta, timezone
 from appointment.models import Appointment
 from appointment.libs import brasil_api
 
+
 def get_available_times(date: date) -> Iterable[datetime]:
 
     if  brasil_api.is_holiday(date):
@@ -17,6 +18,24 @@ def get_available_times(date: date) -> Iterable[datetime]:
 
     while start < end:
         if not Appointment.objects.filter(date_time=start).exists():
+            available_times.add(start)
+        start += delta
+
+    return available_times
+
+def get_available_times_by_provider(provider, date: date) -> Iterable[datetime]:
+
+    if  brasil_api.is_holiday(date):
+        return []
+
+    start = datetime(year=date.year, month=date.month, day=date.day, hour=9, minute=0, tzinfo=timezone.utc)
+    end = datetime(year=date.year, month=date.month, day=date.day, hour=18, minute=0, tzinfo=timezone.utc)
+    delta = timedelta(minutes=30)
+
+    available_times = set()
+    
+    while start < end:
+        if not Appointment.objects.filter(provider=provider,date_time=start).exists():
             available_times.add(start)
         start += delta
 
